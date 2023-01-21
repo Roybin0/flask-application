0925e0f9f4a407f5445b9e905ca6a9a566f65361
Robyn Smith <robyn.roybin@gmail.com>
2023-01-21 15:55:46 +0000

    Add new task functionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,3 +2,16 @@ document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
   });
+
+  document.addEventListener('DOMContentLoaded', function () {
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmm, yyyy", 
+      i18n: {done: "Select"}
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+  });
